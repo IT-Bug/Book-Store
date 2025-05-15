04cfc0ad514f6abe5402740c4165525302a30a3c
IT-Bug <Kevin-Kirchmeer@gmx.de>
2025-05-15 17:59:30 +0200

    Pasted new function and template

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function renderPrice(){
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = "";
+    books.findIndex((price) => { return price["price"] == true});
+    
+    for (let indexPrice = 0; indexPrice < books.length; indexPrice++) {
+        contentRef.innerHTML += getPriceTemplate(indexPrice);
+    }
+}
